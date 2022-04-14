1040fc73fdadb24cda57c06f44ef4e58c1c3d183
SolveItProject <69324227+SolveItProject@users.noreply.github.com>
2022-04-14 18:05:00 +0200

    Add docstrings for functions

--- plate_detect.py
+++ plate_detect.py
@@ -1,19 +1,43 @@
+"""
+LICENSE PLATE RECOGNITION USING HAAR CASCADE
+
+TO RUN SCRIPT RUNC CMD IN DIRECTORY AND TYPE:
+    python palte_detect.py --input YOUR_PATH_TO_IMAGE
+
+"""
+
 import argparse
 
 import cv2
-import numpy as np
 import matplotlib.pyplot as plt
 
 PATH = None
 PLATE_CASCADE = cv2.CascadeClassifier('haarcascade_russian_plate_number.xml')
 
 def display(img):
+    """
+    CONVERT IMAGE COLOR FROM BGR TO RGB AND DISPLAY IT
+
+    Args:
+        img (numpy.ndarray): Image object to display
+    """
+    
     fig = plt.figure(figsize=(10,8))
     ax = fig.add_subplot(111)
     new_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     ax.imshow(new_img)
+    plt.show()
 
 def detect_plate(img):
+    """
+    DETECT PLATE REGION WITH HAAR CASCADE
+
+    Args:
+        img (numpy.ndarray): Image object to analyse
+
+    Returns:
+        numpy.ndarray: Array of coordinates of detected region(s)
+    """
     global PLATE_CASCADE
   
     plate_img = img.copy()
@@ -22,6 +46,16 @@ def detect_plate(img):
     return plate_rects
 
 def blur_plate(img, plate_rects):
+    """
+    BLUR DETECTED REGION OF IMAGE
+
+    Args:
+        img (numpy.ndarray): Image object to blur
+        plate_rects (numpy.ndarray): Array of coordinates of detected region(s)
+
+    Returns:
+        numpy.ndarray: Imgae object with blurred license plates
+    """
     plate_img = img.copy()
     roi = img.copy()
     
@@ -35,13 +69,28 @@ def blur_plate(img, plate_rects):
     return plate_img
 
 def main(args):
+    """
+    MAIN FUNCTION, READ, DETECT AND BLUR THE PLATE
+
+    Args:
+        args (argparse.Namespace): Parsed argument with input image path
+    """
     global PATH
     
     PATH = args.input
     img = cv2.imread(PATH)
     
+    result = blur_plate(img, detect_plate(img))
+    
+    display(result)
+    
 def parse_arguments():
-    '''PARSE INPUT IMAGE'''
+    """
+    PARSE ARGUMENT WITH PATH OF INPUT IMAGE
+
+    Returns:
+        argparse.Namespace: Parsed argument with input image path
+    """
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', default=None)
@@ -49,3 +98,4 @@ def parse_arguments():
 
 if __name__ == '__main__':
     main(parse_arguments())
+    
